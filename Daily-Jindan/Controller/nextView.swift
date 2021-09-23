f5d7bbdb97be4dac558becb08ad81d4ea7707679
pasin7793 <pasin7793@gmail.com>
2021-09-23 23:14:09 +0900

    질문 추가,버튼 추가

--- Daily-Jindan/Controller/nextView.swift
+++ Daily-Jindan/Controller/nextView.swift
@@ -27,8 +27,11 @@ class nextView: UIViewController{
     let checkBox3Yes = Checkbox()
     let checkBox3No = Checkbox()
     
-    let temperature1 = UILabel()
-    let temperature2 = UILabel()
+    let no = UILabel()
+    let yes = UILabel()
+    
+    let no1 = UILabel()
+    let yes1 = UILabel()
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,10 +50,25 @@ class nextView: UIViewController{
         view.addSubview(q1)
         view.addSubview(q2)
         view.addSubview(q3)
-        view.addSubview(temperature1)
-        view.addSubview(temperature2)
+        view.addSubview(no)
+        view.addSubview(yes)
+        view.addSubview(yes1)
+        view.addSubview(no1)
     }
     func setLayout(){
+        q1.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(bounds.height*0.35)
+            make.width.equalTo(bounds.width*0.9)
+            make.height.equalTo(bounds.height*0.14)
+        }
+        q2.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(checkBox1Yes.snp.bottom).offset(bounds.height*0.08)
+            make.width.equalTo(bounds.width*0.9)
+            make.height.equalTo(bounds.height*0.12)
+        }
+        ///
         checkBox1Yes.snp.makeConstraints { make in
             make.height.equalTo(bounds.height*0.04)
             make.width.equalTo(bounds.height*0.04)
@@ -63,23 +81,44 @@ class nextView: UIViewController{
             make.top.equalTo(checkBox1Yes.snp.bottom).offset(bounds.height*0.015)
             make.left.equalTo(bounds.width*0.1)
         }
-        q1.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(bounds.height*0.35)
-            make.width.equalTo(bounds.width*0.9)
-            make.height.equalTo(bounds.height*0.14)
+        ///
+        checkBox2Yes.snp.makeConstraints { make in
+            make.height.equalTo(bounds.height*0.04)
+            make.width.equalTo(bounds.height*0.04)
+            make.top.equalTo(q2.snp.bottom).offset(bounds.height*0.02)
+            make.left.equalTo(bounds.width*0.1)
         }
-        temperature1.snp.makeConstraints { make in
+        checkBox2No.snp.makeConstraints { make in
+            make.height.equalTo(bounds.height*0.04)
+            make.width.equalTo(bounds.height*0.04)
+            make.top.equalTo(checkBox2Yes.snp.bottom).offset(bounds.height*0.015)
+            make.left.equalTo(bounds.width*0.1)
+        }
+        ///
+        no.snp.makeConstraints { make in
             make.height.equalTo(bounds.height*0.2)
             make.width.equalTo(bounds.width*0.25)
             make.top.equalTo(checkBox1Yes.snp.bottom).offset(bounds.height*(-0.12))
             make.left.equalTo(checkBox1Yes.snp.right).offset(bounds.width*0.04)
         }
-        temperature2.snp.makeConstraints { make in
+        yes.snp.makeConstraints { make in
             make.height.equalTo(bounds.height*0.2)
             make.width.equalTo(bounds.width*0.6)
-            make.top.equalTo(temperature1.snp.bottom).offset(bounds.height*(-0.145))
+            make.top.equalTo(no.snp.bottom).offset(bounds.height*(-0.145))
             make.left.equalTo(checkBox1Yes.snp.right).offset(bounds.width*0.04)
+        }
+        ///
+        no1.snp.makeConstraints { make in
+            make.height.equalTo(bounds.height*0.2)
+            make.width.equalTo(bounds.width*0.25)
+            make.top.equalTo(checkBox2Yes.snp.bottom).offset(bounds.height*(-0.12))
+            make.left.equalTo(checkBox2Yes.snp.right).offset(bounds.width*0.04)
+        }
+        yes1.snp.makeConstraints { make in
+            make.height.equalTo(bounds.height*0.2)
+            make.width.equalTo(bounds.width*0.6)
+            make.top.equalTo(no1.snp.bottom).offset(bounds.height*(-0.145))
+            make.left.equalTo(checkBox2Yes.snp.right).offset(bounds.width*0.04)
         }
     }
     func configureUI(){
@@ -91,9 +130,13 @@ class nextView: UIViewController{
         
         checkBox1Yes.borderStyle = .circle
         checkBox1Yes.checkmarkStyle = .circle
-        
         checkBox1No.borderStyle = .circle
         checkBox1No.checkmarkStyle = .circle
+        
+        checkBox2Yes.borderStyle = .circle
+        checkBox2Yes.checkmarkStyle = .circle
+        checkBox2No.borderStyle = .circle
+        checkBox2No.checkmarkStyle = .circle
         
         q1.font = UIFont(name: "Helvetica", size: 17)
         q1.backgroundColor = UIColor(red: 0.8863, green: 1, blue: 0.9686, alpha: 1.0)
@@ -101,13 +144,22 @@ class nextView: UIViewController{
         q1.isSelectable = false
         q1.isEditable = false
         
-        temperature1.text = "37.5℃ 미만"
-        temperature2.text = "37.5℃ 이상 및 열감지"
-        temperature1.textColor = .gray
-        temperature2.textColor = .gray
+        q2.font = UIFont(name: "Helvetica", size: 17)
+        q2.backgroundColor = UIColor(red: 0.8863, green: 1, blue: 0.9686, alpha: 1.0)
+        q2.text = "2. 학생 본인 또는 동거인이 코로나19 진단검사를 받고 그 결과를 기다리고 있나요?※ ① 직업특성, 또는 ② 대회참여 등 선제적 예방 목적의 진단검사인 경우는 ‘아니오’ 선택"
+        q2.isSelectable = false
+        q2.isEditable = false
+        
+        no.text = "아니요"
+        yes.text = "예"
+        no.textColor = .gray
+        yes.textColor = .gray
+        
+        no1.text = "아니요"
+        yes1.text = "예"
+        no1.textColor = .gray
+        yes1.textColor = .gray
     }
-    
-    
     // MARK: - Actions
     @objc func tappedButton(){
         
